00753c31dc510f740f8799d3dcfc80a87481aa1c
Jonathan Rice <jottorice@gmail.com>
2017-12-23 22:29:27 -0600

    Add cross-origin request code to items router.

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -12,6 +12,12 @@ itemRouter.use(bodyParser.json());
 //var Verify = require('./verify');
 
 itemRouter.route('/')
+.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ })
+
 .get(function (req, res, next) {
     console.log('In .get');
     Items.find(req.query)
